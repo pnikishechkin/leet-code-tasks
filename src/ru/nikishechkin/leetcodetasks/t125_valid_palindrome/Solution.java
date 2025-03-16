008dc9e339ee93759f2c33a53ed308b621244a80
pnikishechkin <pnikisheckin@MacBook-Pro-Petr.local>
2025-03-16 22:27:01 +0300

    125. Valid palindrome

--- src/ru/nikishechkin/leetcodetasks/t125_valid_palindrome/Solution.java
+++ src/ru/nikishechkin/leetcodetasks/t125_valid_palindrome/Solution.java
@@ -1,6 +1,8 @@
 package ru.nikishechkin.leetcodetasks.t125_valid_palindrome;
 
 /**
+ * 125. Valid palindrome
+ *
  * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing
  * all non-alphanumeric characters, it reads the same forward and backward.
  * Alphanumeric characters include letters and numbers.
@@ -13,12 +15,20 @@ package ru.nikishechkin.leetcodetasks.t125_valid_palindrome;
  */
 public class Solution {
     public static void main(String[] args) {
-        isPalindrome("A man, a plan, a canal: Panama");
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
     }
 
     public static boolean isPalindrome(String s) {
-        String obr = s.toLowerCase().replaceAll("[^a-zA-Z\\s]", "");
-        System.out.println(obr);
+        // любой символ, кроме символьных
+        String regex = "[^a-zA-Z\\d]";
+        String obr = s.toLowerCase().replaceAll(regex, "");
+        int i = 0;
+        while (i < obr.length() / 2) {
+            if (obr.charAt(i) != obr.charAt(obr.length() - i - 1)) {
+                return false;
+            }
+            i++;
+        }
         return true;
     }
 }
