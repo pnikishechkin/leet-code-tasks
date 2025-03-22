95919a20fcaf3d05c59b1eed280900c55a8b3dd6
pnikishechkin <pnikishechkin@yandex.ru>
2025-03-22 22:21:23 +0300

    383. Ransom note

--- src/t383_ransom_note/Solution.java
+++ src/t383_ransom_note/Solution.java
@@ -1,4 +1,27 @@
 package t383_ransom_note;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader("test.txt"))) {
+            Scanner sc = new Scanner(reader);
+            String s = sc.next();
+            System.out.println(s);
+        } catch (FileNotFoundException e) {
+            System.out.println("Ошибка чтения BufferedReader");
+        }
+
+        try {
+            List<String> lines = Files.readAllLines(Path.of("test.txt"));
+            System.out.println(lines.toString());
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения Files");
+        }
+    }
 }
